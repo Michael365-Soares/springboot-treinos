530c48f6b11296bc7eb9e6fc964801e7de6017a7
Michael S Oliveira <61204252+Michael365-Soares@users.noreply.github.com>
2022-07-07 13:33:49 -0300

    Implementando métodos de criação de um CRUD...

--- revisoes-spring/src/main/java/br/com/michaelsoares/revisoesspring/controller/ProdutoController.java
+++ revisoes-spring/src/main/java/br/com/michaelsoares/revisoesspring/controller/ProdutoController.java
@@ -3,6 +3,8 @@ package br.com.michaelsoares.revisoesspring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -10,12 +12,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.michaelsoares.revisoesspring.model.entitys.Produto;
 import br.com.michaelsoares.revisoesspring.model.repositories.ProdutoRepository;
 
 @Controller
+@RestController
 @RequestMapping("produtos")
 public class ProdutoController {
     @Autowired
@@ -43,6 +48,51 @@ public class ProdutoController {
     	List<Produto> lista=produtoRepository.findByPrecoBetween(inicial, fim);
     	model.addAttribute("lista",lista);
     	return new ModelAndView("produto/produtoCadastrado2");
+    }
+    	
+    @GetMapping("/consultaPaginada")
+    public ModelAndView consultaPaginada(ModelMap model){
+    	Pageable page=PageRequest.ofSize(10);
+    	Iterable<Produto> produtos=produtoRepository.findAll(page);
+    	model.addAttribute("produtos",produtos);
+    	return new ModelAndView("produto/resultPaginado");
+    }
+    
+    @GetMapping("/pesquisarPorId")
+    public ModelAndView pesquisarPorId(Model model) {
+    	return new ModelAndView("produto/pesquisarPorId");
+    }
+    @PostMapping("/resultPesquisaId")
+    public ModelAndView resultPesquisaId(int id,Model model) {
+    	Produto p=produtoRepository.findById(id);
+    	model.addAttribute("produto",p);
+    	return new ModelAndView("produto/resultPesquisaId");
+    }
+    
+    @GetMapping("/alterarProduto")
+    public ModelAndView alterarProduto() {
+    	return new ModelAndView("produto/alterarProduto");
+    }
+    @PostMapping("/prodAtualizado")
+    public ModelAndView prodAtualizado(Model model,@ModelAttribute Produto produto) {
+    	produtoRepository.save(produto);
+    	model.addAttribute("produto", produto);
+    	return new ModelAndView("produto/prodAtualizado");
+    }
+    
+    
+    @GetMapping("excluir")
+    public ModelAndView excluir() {
+    	return new ModelAndView("produto/excluirProduto");
+    }
+    
+    @PostMapping("/excluirPorId")
+    public ModelAndView excluirPorId(@RequestParam int id,Model model) {
+    	produtoRepository.deleteById(id);
+    	Pageable page=PageRequest.ofSize(10);
+    	Iterable<Produto> produtos=produtoRepository.findAll(page);
+    	model.addAttribute("produtos",produtos);
+    	return new ModelAndView("produto/resultPaginado");
     }
     
 }
